9ec93dfd47847e1039f0bd180e408bcbacccebe5
W4CKY <163338407+W4CKYFR@users.noreply.github.com>
2025-11-21 22:12:09 +0200

    Update cgl_window.hpp

--- include/cgl_window.hpp
+++ include/cgl_window.hpp
@@ -1,23 +1,26 @@
 #pragma once
 #include <string>
 #include <GLFW/glfw3.h>
+#include "cgl.hpp"
+#include <nanovg.h>
 
 namespace cgl {
-	class Window {
-	public:
-		Window(int width, int height, const std::string& title);
-		~Window();
+    class Window {
+    public:
+        Window(int width, int height, const std::string& title);
+        ~Window();
 
-		bool ShouldClose() const;
-		void PollEvents() const;
-		void SwapBuffers() const;
-		void clearColorBufferBit() const;
-		void setColor(float Red, float Blue, float Green, float alpha) const;
-		GLFWwindow* GetHandle() const;
-		void Close() const;
+        bool ShouldClose() const;
+        void PollEvents() const;
+        void SwapBuffers() const;
+        void clearColorBufferBit() const;
+        void setColor(float Red, float Blue, float Green, float alpha) const;
+        GLFWwindow* GetHandle() const;
+        void Close() const;
+        NVGcontext* GetVGContext() const;
 
-	private:
-		GLFWwindow* handle;
-
-	};
+    private:
+        NVGcontext* vg = nullptr;
+        GLFWwindow* handle = nullptr;
+    };
 }
